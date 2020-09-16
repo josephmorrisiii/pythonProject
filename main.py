c4892ba6d156fb0224b60938bd2be955c864c6cf
Joseph Morris III <Joseph.morrisiii@hotmail.com>
2020-09-16 11:42:50 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,14 +3,6 @@
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('Enter integer:')
-    usersFirstInteger = int(input())
-    print('You entered:', usersFirstInteger)
-
-    print(usersFirstInteger, 'squared is', usersFirstInteger ** 2)
-    print('And', usersFirstInteger, 'cubed is', usersFirstInteger ** 3, '!!')
-
-    print('Enter another integer:')
-    userSecondInteger = int(input())
-    print(usersFirstInteger, '+', userSecondInteger, 'is', usersFirstInteger+userSecondInteger )
-    print(usersFirstInteger, '*', userSecondInteger, 'is', usersFirstInteger*userSecondInteger )
+    userNum = int(input())
+    userNumSquared = userNum ** 2
+print(userNumSquared)
