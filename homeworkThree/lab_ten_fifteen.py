a8cf870db047a136ac806831f4f74b23a3cf4bf5
Joseph Morris III <Joseph.morrisiii@hotmail.com>
2020-11-01 21:43:09 -0600

    Update lab_ten_fifteen.py

--- homeworkThree/lab_ten_fifteen.py
+++ homeworkThree/lab_ten_fifteen.py
@@ -1,4 +1,4 @@
-##Joseph Morris
+#Joseph Morris
 #1840300
 
 class Team:
@@ -10,30 +10,30 @@ class Team:
     def get_win_percentage(self):
         return self.team_wins / (self.team_wins + self.team_losses)
 
-    def set_team_losses(self, team_losses):
+    def get_team_losses(self, team_losses):
         self.team_losses = team_losses
 
-    def set_team_wins(self, team_wins):
+    def get_team_wins(self, team_wins):
         self.team_wins = team_wins
 
-    def set_team_name(self, team_name):
+    def get_team_name(self, team_name):
         self.team_name = team_name
 
 
 if __name__ == '__main__':
 
-    student_team = Team()
+    professional_team = Team()
 
     team_name = input()
     team_wins = int(input())
     team_losses = int(input())
 
-    student_team.set_team_name(team_name)
-    student_team.set_team_wins(team_wins)
-    student_team.set_team_losses(team_losses)
+    professional_team.get_team_name(team_name)
+    professional_team.get_team_wins(team_wins)
+    professional_team.get_team_losses(team_losses)
 
-    if student_team.get_win_percentage() >= 0.5:
-        print('Congratulations, Team', student_team.team_name, 'has a winning average!')
+    if professional_team.get_win_percentage() >= 0.5:
+        print('Congratulations, Team', professional_team.team_name, 'has a winning average!')
     else:
-        print('Team', student_team.team_name, 'has a losing average.')
+        print('Team', professional_team.team_name, 'has a losing average.')
 
