9683f9a5bf55656a183ba1c33b869cb9b99a80b8
Joseph Morris III <Joseph.morrisiii@hotmail.com>
2020-11-01 21:10:47 -0600

    Update lab_ten_eleven.py

--- homeworkThree/lab_ten_eleven.py
+++ homeworkThree/lab_ten_eleven.py
@@ -3,11 +3,11 @@
 
 
 class FoodItem:
-    def __init__(self, name='None',fat=0.0, carbs=0.0,protein=0.0):
-        self.name=name
-        self.fat=fat
-        self.carbs=carbs
-        self.protein=protein
+    def __init__(self, name='None', fat=0.0, carbs=0.0, protein=0.0):
+        self.name = name
+        self.fat = fat
+        self.carbs = carbs
+        self.protein = protein
 
     def get_calories(self, num_servings):
         # Calorie formula
@@ -20,8 +20,9 @@ class FoodItem:
         print('   Carbohydrates: {:.2f} g'.format(self.carbs))
         print('   Protein: {:.2f} g'.format(self.protein))
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
+
     food_number_one = FoodItem()
 
     food_name = input()
@@ -31,13 +32,10 @@ if __name__ == '__main__':
 
     food_number_two = FoodItem(food_name, food_fat, food_carbs, food_protein)
     how_many_servings = float(input())
-
     food_number_one.print_info()
-
     print('Number of calories for {:.2f} serving(s): {:.2f}'.format(how_many_servings,
                                                                     food_number_one.get_calories(how_many_servings)))
     print()
-
     food_number_two.print_info()
     print('Number of calories for {:.2f} serving(s): {:.2f}'.format(how_many_servings,
                                                                     food_number_two.get_calories(how_many_servings)))
