643495ef25c0b3d785c126fd550b26b4d51a0b8d
Joseph Morris III <Joseph.morrisiii@hotmail.com>
2020-11-01 21:22:45 -0600

    Update lab_ten_seventeen.py

--- homeworkThree/lab_ten_seventeen.py
+++ homeworkThree/lab_ten_seventeen.py
@@ -2,35 +2,36 @@
 #1840300
 
 class ItemToPurchase:
-     def __init__(self):
+     def __init__(self, item_name = 'none', item_price = 0, item_quantity = 0):
         self.item_name = "none"
         self.item_price = 0
         self.item_quantity = 0
 
      def print_item_cost(self):
         print(self.item_name + " " + str(self.item_quantity) + " @ $"
-              + str(self.item_price) + " = $" + str(self.item_price * self.item_quantity))
+              + str(self.item_price) +
+              " = $" + str(self.item_price * self.item_quantity))
 
 
 if __name__ == "__main__":
-        print("Item 1")
-        item1 = ItemToPurchase()
-        item2 = ItemToPurchase()
+    print("Item 1")
+    item_one = ItemToPurchase()
+    item_two = ItemToPurchase()
 
-        item1.item_name = input('Enter the item name:\n')
-        item1.item_price = int(input('Enter the item price:\n'))
-        item1.item_quantity = int(input('Enter the item quantity:\n'))
-        print("\nItem 2")
+    item_one.item_name = input('Enter the item name:\n')
+    item_one.item_price = int(input('Enter the item price:\n'))
+    item_one.item_quantity = int(input('Enter the item quantity:\n'))
 
-        item2.item_name = input('Enter the item name:\n')
-        item2.item_price = int(input('Enter the item price:\n'))
-        item2.item_quantity = int(input('Enter the item quantity:\n'))
+    print("\nItem 2")
+    item_two.item_name = input('Enter the item name:\n')
+    item_two.item_price = int(input('Enter the item price:\n'))
+    item_two.item_quantity = int(input('Enter the item quantity:\n'))
 
-        print("\nTOTAL COST")
+    print("\nTOTAL COST")
 
-        item1.print_item_cost()
-        item2.print_item_cost()
+    item_one.print_item_cost()
+    item_two.print_item_cost()
 
-        total = (item1.item_price * item1.item_quantity) + (item2.item_price * item2.item_quantity)
+    total_cost_of_items = (item_one.item_price * item_one.item_quantity) + (item_two.item_price * item_two.item_quantity)
 
-        print("\nTotal: $" + str(total))
+    print("\nTotal: $" + str(total_cost_of_items))
